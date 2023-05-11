ddb501487752fb67f15463138f24ef5e7ce68d2c
Variapolis <tmarksinbox@gmail.com>
2023-05-11 06:01:43 +0100

    feat(home): add additional info to home page

--- CourseWork2Starter/WeatherViews/Home.swift
+++ CourseWork2Starter/WeatherViews/Home.swift
@@ -16,72 +16,75 @@ struct Home: View {
     
     
     var body: some View {
-        VStack {
-            Spacer()
-            Text("This is the initial view when the app launches, that must show  weather data as in Figure 1.\nInitial data is loaded from file on launch and a change of location should should update the weather data.\nBuild this view so that it mirrors this image")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.center)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            Text("Refer to Figure 1 for weather elements that must be rendered.")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .multilineTextAlignment(.center)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            HStack {
+        ZStack{
+            // Background Image
+            VStack {
+                HStack {
+                    Button {
+                        self.isSearchOpen.toggle()
+                    } label: {
+                        Text("Change Location")
+                            .bold()
+                            .font(.system(size: 30))
+                    }
+                    .padding(.top, 35.0)
+                    .sheet(isPresented: $isSearchOpen) {
+                        SearchView(isSearchOpen: $isSearchOpen, userLocation: $userLocation)
+                    }
+                }
+                
                 Spacer()
                 
-                Button {
-                    self.isSearchOpen.toggle()
-                } label: {
-                    Text("Change Location")
-                        .bold()
-                        .font(.system(size: 30))
-                }
-                .sheet(isPresented: $isSearchOpen) {
-                    SearchView(isSearchOpen: $isSearchOpen, userLocation: $userLocation)
-                }
+                Text(userLocation)
+                    .font(.title)
+                    .foregroundColor(.black)
+                    .shadow(color: .black, radius: 0.5)
+                    .multilineTextAlignment(.center)
+                
+                Text(Date(timeIntervalSince1970: TimeInterval(((Int)(modelData.forecast?.current.dt ?? 0))))
+                    .formatted(.dateTime.year().hour().month().day()))
                 .padding()
+                .font(.largeTitle)
+                .foregroundColor(.black)
+                .shadow(color: .black, radius: 1)
                 
                 Spacer()
+                
+                Text("Temp: \((Int)(modelData.forecast!.current.temp))ºC")
+                    .padding()
+                    .font(.title2)
+                    .foregroundColor(.black)
+                    .shadow(color: .black, radius: 0.5)
+                
+                Text("Humidity: \(modelData.forecast!.current.humidity)%")
+                    .padding()
+                    .font(.title2)
+                    .foregroundColor(.black)
+                    .shadow(color: .black, radius: 0.5)
+                
+                Text("Pressure: \(modelData.forecast!.current.pressure)hPa")
+                    .padding()
+                    .font(.title2)
+                    .foregroundColor(.black)
+                    .shadow(color: .black, radius: 0.5)
+                
+                HStack{
+                    // Image Goes here!
+                    Text(modelData.forecast!.current.weather.first!.weatherDescription.rawValue.capitalized)
+                        .font(.title2)
+                        .foregroundColor(.black)
+                        .shadow(color: .black, radius: 0.5)
+                }
+                Spacer()
             }
-            
-            Text(userLocation)
-                .font(.title)
-                .foregroundColor(.black)
-                .shadow(color: .black, radius: 0.5)
-                .multilineTextAlignment(.center)
-            
-            Text(Date(timeIntervalSince1970: TimeInterval(((Int)(modelData.forecast?.current.dt ?? 0))))
-                .formatted(.dateTime.year().hour().month().day()))
-            .padding()
-            .font(.largeTitle)
-            .foregroundColor(.black)
-            .shadow(color: .black, radius: 1)
-            
-            Spacer()
-            
-            Text("Temp: \((Int)(modelData.forecast!.current.temp))ºC")
-                .padding()
-                .font(.title2)
-                .foregroundColor(.black)
-                .shadow(color: .black, radius: 0.5)
-            
-            Text("The tab bar navigation must be re-engineered\n to mirror what has been shown in all the images.")
-                .font(.subheadline)
-                .fontWeight(.semibold)
-                .padding(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
-            
-            
-            Spacer()
-        }
-        .onAppear {
-            Task.init {
-                self.userLocation = await getLocFromLatLong(lat: modelData.forecast!.lat, lon: modelData.forecast!.lon)
+            .onAppear {
+                Task.init {
+                    self.userLocation = await getLocFromLatLong(lat: modelData.forecast!.lat, lon: modelData.forecast!.lon)
+                    
+                    
+                }
                 
             }
-            
         }
-        
     }
 }
